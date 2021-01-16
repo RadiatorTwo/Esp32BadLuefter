9f2c135b653aec7684fae3581627d3f40fff3948
o.roemmich <otto.roemmich@ebbinghausteam.de>
2021-01-16 19:25:38 +0100

    Anpassungen am Design der Oberfläche

--- Esp32BadLuefter.ino
+++ Esp32BadLuefter.ino
@@ -29,7 +29,7 @@ TaskHandle_t Task1;
 #define FAN_MINUTES 10
 #define ACT_INTERVAL 30
 #define CHECK_DELAY 1000
-
+#define LONG_RUN_MINUTES 60
 #define SEALEVELPRESSURE_HPA (1013.25)
 
 const char* host = "esp32";
@@ -44,6 +44,7 @@ bool fanRunning;
 int timeCounter;
 int intervalCounter;
 int humidityCount;
+bool longrun;
 
 //===============================================================
 // Netzwerk Konfiguration
@@ -60,6 +61,19 @@ IPAddress secondaryDNS(192, 168, 1, 1); //optional
 //===============================================================
 AsyncWebServer server(80);
 Adafruit_BME280 bme;
+
+//===============================================================
+// Hilfsfunktionen
+//===============================================================
+void secondsToHMS(int seconds, int &h, int &m, int &s)
+{
+  uint32_t t = seconds;
+  s = t % 60;
+  t = (t - s) / 60;
+  m = t % 60;
+  t = (t - m) / 60;
+  h = t;
+}
 
 //===============================================================
 // HTML Funktionen
@@ -82,14 +96,28 @@ void handleGetData(AsyncWebServerRequest *request)
   if (fanRunning)
   {
     fanValue = "true";
-    timeLeft = (FAN_MINUTES * 60) - timeCounter;
+    if (longrun)
+    {
+      timeLeft = (LONG_RUN_MINUTES * 60) - timeCounter;
+    }
+    else
+    {
+      timeLeft = (FAN_MINUTES * 60) - timeCounter;
+    }
   }
   else
   {
     fanValue = "false";
   }
 
-  String adcValue = String(temperature) + " °C," + String(humidity) + " %," + String(pressure) + " hPa," + String(height) + " m," + fanValue + "," + String(timeLeft);
+  int hours;
+  int minutes;
+  int seconds;
+
+  secondsToHMS(timeLeft, hours, minutes, seconds);
+  
+  String timeValue = String(minutes) + " Minuten " + String(seconds) + " Sekunden verbleibend";
+  String adcValue = String(temperature) + " °C," + String(humidity) + " %," + String(pressure) + " hPa," + String(height) + " m," + fanValue + "," + timeValue;
   request->send(200, "text/plain", adcValue);
 }
 
@@ -110,14 +138,20 @@ void handleResetFan(AsyncWebServerRequest *request)
   request->send(200);
 }
 
+void handleLongrunFan(AsyncWebServerRequest *request)
+{
+  longrun_fan();
+  request->send(200);
+}
+
 static void handleUpload(AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final)
 {
-  if (!index){
+  if (!index) {
     Serial.println("Starte Update");
     Serial.println("Beende Task auf Core 0");
     vTaskDelete(Task1);
     disableCore0WDT();
-    
+
     if (!Update.begin(UPDATE_SIZE_UNKNOWN))
     {
       Update.printError(Serial);
@@ -171,17 +205,37 @@ void checkHumidity()
       intervalCounter = 0;
     }
 
-    if (timeCounter >= FAN_MINUTES * 60)
+    if (longrun)
     {
-      fanRunning = false;
-      intervalCounter = 0;
+      if (timeCounter >= LONG_RUN_MINUTES * 60)
+      {
+        longrun = false;
+        fanRunning = false;
+        intervalCounter = 0;
+      }
+      else
+      {
+        int timeLeft = (LONG_RUN_MINUTES * 60) - timeCounter;
+        Serial.print("Fan still running for ");
+        Serial.print(timeLeft);
+        Serial.println(" seconds.");
+      }
     }
     else
     {
-      int timeLeft = (FAN_MINUTES * 60) - timeCounter;
-      Serial.print("Fan still running for ");
-      Serial.print(timeLeft);
-      Serial.println(" seconds.");
+      if (timeCounter >= FAN_MINUTES * 60)
+      {
+        longrun = false;
+        fanRunning = false;
+        intervalCounter = 0;
+      }
+      else
+      {
+        int timeLeft = (FAN_MINUTES * 60) - timeCounter;
+        Serial.print("Fan still running for ");
+        Serial.print(timeLeft);
+        Serial.println(" seconds.");
+      }
     }
   }
   else
@@ -220,7 +274,18 @@ void activate_fan()
 
 void reset_fan()
 {
+  longrun = false;
   fanRunning = false;
+}
+
+void longrun_fan()
+{
+  digitalWrite(FAN_PIN, HIGH);               // GET /H turns the LED on
+  delay(1000);
+  digitalWrite(FAN_PIN, LOW);
+  longrun = true;
+  fanRunning = true;
+  timeCounter = 0;
 }
 
 //===============================================================
@@ -230,10 +295,13 @@ void setup()
 {
   Serial.begin(115200);
 
+  Serial.println("Starting Device");
+
   pinMode(FAN_PIN, OUTPUT);
   digitalWrite(FAN_PIN, LOW);
 
   unsigned status;
+  longrun = false;
   fanRunning = false;
   humidityCount = 0;
 
@@ -244,11 +312,11 @@ void setup()
   bme.readHumidity();
   bme.readPressure() / 100.0F;
   bme.readAltitude(SEALEVELPRESSURE_HPA);
-  
+
   if (!status) {
     Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
-    Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(), 16);
-    while (1) delay(10);
+    Serial.println("SensorID was: 0x"); Serial.println(bme.sensorID(), 16);
+    Serial.println("Continuing with boot");
   }
 
   Serial.println();
@@ -258,7 +326,7 @@ void setup()
   {
     Serial.println("STA Failed to configure static IP address");
   }
-  
+
   Serial.print("Connecting to ");
   Serial.println(ssid);
 
@@ -291,11 +359,12 @@ void setup()
 
   server.on("/activate_fan", handleActivateFan);
   server.on("/reset_fan", handleResetFan);
+  server.on("/longrun_fan", handleLongrunFan);
 
   /*handling uploading firmware file */
-  server.on("/upload", HTTP_POST, [](AsyncWebServerRequest *request){
-        request->send(200);
-    }, handleUpload);
+  server.on("/upload", HTTP_POST, [](AsyncWebServerRequest * request) {
+    request->send(200);
+  }, handleUpload);
 
   //create a task that will be executed in the Task1code() function, with priority 1 and executed on core 0
   xTaskCreatePinnedToCore(
--- index.h
+++ index.h
@@ -1,31 +1,72 @@
 const char MAIN_page[] PROGMEM = R"=====(
 <!DOCTYPE html>
 <html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <head>
-	<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+  <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
 </head>
 <style>
 .card{
-    max-width: 400px;
-     min-height: 250px;
-     background: #02b875;
-     padding: 30px;
-     box-sizing: border-box;
-     color: #FFF;
-     margin:20px;
-     box-shadow: 0px 2px 18px -4px rgba(0,0,0,0.75);
+  background: #555555;
+  padding: 30px;
+  box-sizing: border-box;
+  color: #FFF;
+  margin: auto;
+  box-shadow: 0px 2px 18px -4px rgba(0,0,0,0.75);
+  width: 100%;
+  position:fixed;
+  top:0;
+  bottom:0;
+  left:0;
+  right:0;
+}
+html{
+    width:100%;
+    height:100%;
+}
+body{
+  width:100%;
+  height:100%;
+  background-color:#DDD;
+  text-align: center;
+}
+button{
+  height: 50px;
+  width: 50%;
+  background-color: #555555;
+  border: 2px solid #FFF;
+  color: white;
+  padding: 15px 32px;
+  margin: 10px auto;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  border-radius: 4px;
+  transition-duration: 0.4s;
+  display: block;
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
+}
+button:hover {
+  background-color: #aaa;
+  color: white;
+}
+a{
+  color: #FFF;
 }
 </style>
 <body>
 <div class="card">
   <h1>ESP32 Main Page</h1>
   <h4>L&uuml;fter an: <span id="fan_on"></span></h4>
+  <h4><span id="remaining_time"></span></h4>
   <h4>Temperatur: <span id="temperature">0%</span></h4>
   <h4>Luftfeuchtigkeit: <span id="humidity">0%</span></h4>
   <h4>Barometrischer Druck: <span id="pressure">0</span></h4>
   <h4>H&ouml;henmeter: <span id="height">0</span></h4>
   <button type="button" id="activate">L&uuml;fter starten</button>
   <button type="button" id="reset">L&uuml;fter Reset</button>
+  <button type="button" id="long_run">60 minuten</button>
   <br><br><a href="/update">Firmware Update</a>
 </div>
 <script>
@@ -39,20 +80,22 @@ function getData() {
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200)
     {
-	    var data = this.responseText.split(",");
+      var data = this.responseText.split(",");
       document.getElementById("temperature").innerHTML = data[0];
-	    document.getElementById("humidity").innerHTML = data[1];
+      document.getElementById("humidity").innerHTML = data[1];
       document.getElementById("pressure").innerHTML = data[2];
       document.getElementById("height").innerHTML = data[3];
 
       var fanValue = data[4];
       if(fanValue === "true")
       {
-        document.getElementById("fan_on").innerHTML = "Ja " + data[5] + " Sekunden verbleiben";
+        document.getElementById("fan_on").innerHTML = "Ja";
+        document.getElementById("remaining_time").innerHTML = data[5];
       }
       else
       {
         document.getElementById("fan_on").innerHTML = "Nein";
+        document.getElementById("remaining_time").innerHTML = "";
       }
     }
   };
@@ -74,6 +117,14 @@ $(function()
   {
     $.ajax({
       url: '/reset_fan',
+      type: 'POST'
+      });
+  });
+
+  $('#long_run').click(function()
+  {
+    $.ajax({
+      url: '/longrun_fan',
       type: 'POST'
       });
   });
