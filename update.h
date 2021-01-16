16738578f6e16880fd668e07ba35e6921cb9c396
o.roemmich <otto.roemmich@ebbinghausteam.de>
2021-01-16 19:25:38 +0100

    Anpassungen am Design der Oberfläche

--- update.h
+++ update.h
@@ -1,19 +1,37 @@
 const char UPDATE_page[] PROGMEM = R"=====(
 <!DOCTYPE html>
 <html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <head>
 	<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
 </head>
 <style>
 .card{
-    max-width: 400px;
-     min-height: 250px;
-     background: #02b875;
-     padding: 30px;
-     box-sizing: border-box;
-     color: #FFF;
-     margin:20px;
-     box-shadow: 0px 2px 18px -4px rgba(0,0,0,0.75);
+  background: #555555;
+  padding: 30px;
+  box-sizing: border-box;
+  color: #FFF;
+  margin: auto;
+  box-shadow: 0px 2px 18px -4px rgba(0,0,0,0.75);
+  width: 100%;
+  position:fixed;
+  top:0;
+  bottom:0;
+  left:0;
+  right:0;
+}
+html{
+    width:100%;
+    height:100%;
+}
+body{
+  width:100%;
+  height:100%;
+  background-color:#DDD;
+  text-align: center;
+}
+a{
+  color: #FFF;
 }
 </style>
 <body>
@@ -49,6 +67,7 @@ const char UPDATE_page[] PROGMEM = R"=====(
         },
         success: function(d, s) {
             console.log('success!')
+            window.location = '/';
         },
         error: function(a, b, c) {}
     });
